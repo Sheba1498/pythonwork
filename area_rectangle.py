bbbca473e8241a3ace1c8b08f79090fd46931383
sheba <shebakochu14@gmail.com>
2022-06-20 11:32:13 +0530

    adda

---
+++ area_rectangle.py
@@ -0,0 +1,6 @@
+l=int(input('Enter the length'))
+b=int(input('Enter the breadth'))
+Area=l*b
+perimeter=2*(l+b)
+print("The area is",Area)
+print("The perimeter is",perimeter)
---
+++ dictionaryworks/accounts.py
@@ -0,0 +1,68 @@
+accounts = [
+    {
+        "acno": 1000, "ac_type": "savings", "balance": 5000, "transactions": [
+        {"to": 1001, "amount": 500, "note": "ebill", "method": "g-pay"},
+        {"to": 1002, "amount": 600, "note": "geto", "method": "neft"},
+        {"to": 1003, "amount": 100, "note": "erchrge", "method": "phone-pay"}
+    ]
+    },
+    {
+        "acno": 1001, "ac_type": "savings", "balance": 6000, "transactions": [
+        {"to": 1000, "amount": 500, "note": "ebill", "method": "g-pay"},
+        {"to": 1002, "amount": 600, "note": "geto", "method": "neft"},
+        {"to": 1003, "amount": 100, "note": "erchrge", "method": "phone-pay"}
+    ]
+    },
+    {
+        "acno": 1002, "ac_type": "current", "balance": 8000, "transactions": [
+        {"to": 1001, "amount": 700, "note": "ebill", "method": "g-pay"},
+        {"to": 1000, "amount": 600, "note": "geto", "method": "neft"},
+        {"to": 1003, "amount": 800, "note": "erchrge", "method": "phone-pay"}
+    ]
+    },
+    {
+        "acno": 1003, "ac_type": "credit", "balance": 15000, "transactions": [
+        {"to": 1001, "amount": 1500, "note": "ebill", "method": "g-pay"},
+        {"to": 1002, "amount": 800, "note": "geto", "method": "neft"},
+        {"to": 1000, "amount": 100, "note": "erchrge", "method": "phone-pay"}
+    ]
+    },
+    {
+        "acno": 1004, "ac_type": "savings", "balance": 50000, "transactions": [
+        {"to": 1001, "amount": 500, "note": "ebill", "method": "g-pay"},
+        {"to": 1002, "amount": 600, "note": "geto", "method": "neft"},
+        {"to": 1003, "amount": 100, "note": "erchrge", "method": "phone-pay"}
+    ]
+    },
+
+]
+
+
+#q1  print details of 1002
+# for ac in accounts:
+#     if ac["acno"]==1002:
+#         transactions=ac.pop("transactions")
+#         print(ac)
+
+ac_details=[ac for ac in accounts if ac["acno"]==1002]
+# print(ac_details)
+
+
+#print savings type account details
+savings_ac=[ac["acno"] for ac in accounts if ac["ac_type"]=="savings"]
+print(savings_ac)
+
+#q3 sort account based on balance on descending order
+accounts.sort(reverse=True, key=lambda ac:ac["balance"])
+print(accounts)
+
+
+#q4 prit all phone pay transactions
+
+
+#q5 prit all transactions where transaction amount > 500
+
+#q6 crredit transactions of 1002
+
+
+#q7 aggregate transactions based on payment mode
